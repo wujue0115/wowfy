0863cc2bf78e1919ad8b3981bbcab7ece29999c3
Wujue <wujue0115@gmail.com>
2024-11-10 16:33:44 +0800

    chore: update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  ignores: [
-    '**/*.md',
-    '**/rollup.config.js',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-expressions': 'off',
+export default antfu(
+  {
+    ignores: [
+      '**/*.md',
+      '**/rollup.config.js',
+    ],
   },
-})
+  {
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+)
