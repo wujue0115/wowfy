7a20174041e2817a78863a62ce37bdaf9927f86f
Wujue <wujue0115@gmail.com>
2024-11-17 16:01:50 +0800

    chore: update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,9 @@ export default antfu(
   {
     rules: {
       '@typescript-eslint/no-unused-expressions': 'off',
+      'style/brace-style': ['error', '1tbs'],
+      'style/nonblock-statement-body-position': ['error', 'beside', { overrides: {} }],
+      'antfu/if-newline': 'off',
     },
   },
 )
